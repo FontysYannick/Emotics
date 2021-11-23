85d9f73927f563ed61bb2bbb85457d69f3f2f920
fontysMatthijs <484020@student.fontys.nl>
2021-11-23 14:53:44 +0100

    test
    test

--- Software/Emotiq/Form1.Designer.cs
+++ Software/Emotiq/Form1.Designer.cs
@@ -48,7 +48,7 @@ namespace Emotiq
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "LLLLll";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -59,7 +59,7 @@ namespace Emotiq
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 

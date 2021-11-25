31356b23402ea97b635839b2c322d84f21c794d8
Fontys_yannick <y.leentvaar@student.fontys.nl>
2021-11-25 14:25:46 +0100

    start homepage
    add name and some tabcontrol stuff

--- Software/Emotiq/Form1.Designer.cs
+++ Software/Emotiq/Form1.Designer.cs
@@ -31,41 +31,67 @@ namespace Emotiq
         {
             this.TabControl = new System.Windows.Forms.TabControl();
             this.Home = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
             this.TabControl.SuspendLayout();
             this.Home.SuspendLayout();
             this.SuspendLayout();
             // 
             // TabControl
             // 
+            this.TabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.TabControl.Controls.Add(this.Home);
             this.TabControl.Controls.Add(this.tabPage2);
             this.TabControl.Controls.Add(this.tabPage1);
             this.TabControl.Location = new System.Drawing.Point(1, 1);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(381, 554);
+            this.TabControl.Size = new System.Drawing.Size(541, 750);
             this.TabControl.TabIndex = 0;
             // 
             // Home
             // 
+            this.Home.BackgroundImage = global::Emotiq.Properties.Resources.Background_emotie;
+            this.Home.Controls.Add(this.label2);
             this.Home.Controls.Add(this.label1);
             this.Home.Location = new System.Drawing.Point(4, 25);
             this.Home.Name = "Home";
             this.Home.Padding = new System.Windows.Forms.Padding(3);
-            this.Home.Size = new System.Drawing.Size(373, 525);
+            this.Home.Size = new System.Drawing.Size(533, 721);
             this.Home.TabIndex = 0;
             this.Home.Text = "Home";
             this.Home.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hoe voelt u zich ?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Goedemiddag Matthijs";
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(373, 525);
+            this.tabPage2.Size = new System.Drawing.Size(533, 721);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Overzich";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -75,26 +101,17 @@ namespace Emotiq
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(373, 525);
+            this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabPage1.Size = new System.Drawing.Size(533, 721);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "Instellingen";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(306, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Goede middag Yannick";
             // 
             // Emotiq
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.ClientSize = new System.Drawing.Size(542, 753);
             this.Controls.Add(this.TabControl);
             this.Name = "Emotiq";
             this.Text = " ";
@@ -113,6 +130,7 @@ namespace Emotiq
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
     }
 }
 

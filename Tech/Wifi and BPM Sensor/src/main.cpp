c5476fa77b8c92f2259bc429d0fdd0c06a0b6e96
oAlwix <alexkreuze@Outlook.com>
2021-11-26 10:04:02 +0100

    Made new file
    New file made which hopefully works

---
+++ Tech/Wifi and BPM Sensor/src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <PulseSensorPlayground.h>
+#include <WiFiNINA.h>
+
+char SSID[] = "MSI9247";
+char PASS[] = "gr3wt2h64";
+int status = WL_IDLE_STATUS;
+
+int PulseSensorPurplePin = 0;
+int Threshold = 550;
+int Signaal;
+
+
+
+void setup() 
+{
+	Serial.begin(9600);
+
+	while (status != WL_CONNECTED)
+  	{
+   		Serial.println("Connecting to WiFi...");
+    	status = WiFi.begin(SSID, PASS);
+    	delay(500);
+  	}
+
+  	Serial.println("Connected to the WiFi network");
+  	Serial.println(WiFi.localIP());
+	Serial.println(Signaal);
+}
+
+void loop() 
+{
+	Signaal = analogRead(PulseSensorPurplePin);
+
+	if (Signaal > Threshold)
+	{
+		Serial.println("High");
+	}
+	else 
+	{
+		Serial.println("Low");
+	}	
+		
+	Serial.println(Signaal);
+	
+	Serial.println(WL_CONNECTED);
+	delay(300);
+}

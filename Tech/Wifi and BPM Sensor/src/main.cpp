a5d3a729250bd0eef9393b0c49bd30a6dab73cd5
oAlwix <alexkreuze@Outlook.com>
2021-11-29 13:45:45 +0100

    random nummer weggehaald
    undid unnecesary changes

--- Tech/Wifi and BPM Sensor/src/main.cpp
+++ Tech/Wifi and BPM Sensor/src/main.cpp
@@ -3,19 +3,8 @@
 #include <WiFiNINA.h>
 using namespace std;
 
-int cout;
-int endl;
 
-int main()
-{
-	for(int x = 1; x<25; x++)
-	{
-		cout << 1+(rand()%6) << endl;
-	}
-}
-
-
-/* char SSID[] = "MSI9247";
+char SSID[] = "MSI9247";
 char PASS[] = "gr3wt2h64";
 int status = WL_IDLE_STATUS;
 
@@ -24,43 +13,35 @@ int Threshold = 550;
 int Signaal;
 
 
+
 void setup() 
 {
 	Serial.begin(9600);
 
 	while (status != WL_CONNECTED)
   	{
-   		Serial.println("Connecting to WiFi...");
-    	status = WiFi.begin(SSID, PASS);
-    	delay(500);
+   		Serial.print("Connecting to WiFi");
+    	delay(200);
+		Serial.print(".");
+		delay(200);
+		Serial.print(".");
+		delay(200);
+		Serial.println(".");
+		delay(200);
+		status = WiFi.begin(SSID, PASS);
   	}
 
-  	Serial.println("Connected to the WiFi network");
-  	Serial.println(WiFi.localIP());
+ 	Serial.println("Connected to the WiFi network");
+ 	Serial.println(WiFi.localIP());
 	delay(1000);
 }
+
+
 
 void loop()
 {
 	Signaal = analogRead(PulseSensorPurplePin);
 
-	Serial.println(Signaal / 10);
-	
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
+	Serial.println(Signaal / 10);	
 	delay(300);
-} */
-
-void setup(){
-	Serial.begin(9600);
-
-
-}
-
-void loop(){
-	Serial.println("Hello World");
-	Serial.println(cout);
-
 }
